ddae9a9d82dba41e8dd4284288e35a0d5000d367
Jorge Miguel <111158100+JorgeMiguelvs@users.noreply.github.com>
2023-03-24 19:44:47 -0300

    Exercicios de aula

---
+++ exercs/src/app/views/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+img1 = 'assets/img2.jpg';
+img2='assets/img3.jpg';
+img3 = 'assets/jpegcomp2.jpg';
+
+css1 = 'st1';
+css2='est2';
+css3 = 'est3';
+}
