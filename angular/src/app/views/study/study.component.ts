ea0c0a45d417152c701276a40a18557ad5c5566a
Jorge Miguel <111158100+JorgeMiguelvs@users.noreply.github.com>
2023-04-11 14:23:51 -0300

    exercicios

--- angular/src/app/views/study/study.component.ts
+++ angular/src/app/views/study/study.component.ts
@@ -139,6 +139,86 @@ mudarNome()
 
 //--
 
+valor1 =0;
+valor2 = 0;
+resultado=0;
+
+operacao(op:string){
+  if(op=="+"){
+    this.resultado= this.valor1 + this.valor2;
+  }else if(op=="-"){
+    this.resultado= this.valor1 - this.valor2;
+  }else if(op=="*"){
+    this.resultado= this.valor1 * this.valor2;
+  }else{
+    this.resultado= this.valor1 / this.valor2;
+  }
+}
+//--
+
+nomeexemplo2 = '';
+
+//--
+botao =false;
+sumirDiv()
+{
+  this.botao = !this.botao;
+}
+
+
+//--
+digitou="";
+
+//--
+numero=0;
+
+
+//--
+campo ='';
+
+//--lista1
+
+nomeAparecer=''
+sobrenomeAparecer=''
+
+//-- 
+
+nomePersonagem ='';
+
+
+perso1='pikachu';
+perso2='charmander';
+perso3='bulbassauro';
+
+//--
+
+contador ='';
+
+contagem(contador: string)
+{
+  return contador.length;
+}
+
+//--
+
+nomeSocial = '';
+nomeUsuario='';
+
+
+//--ngFor
+
+arrayBichos = ['Cachorro', 'Gato', 'Tartaruga', 'Pássaro'];
+
+arrayObj = ['Celular','Notebook','Computador','Garrafa','Mouse','Caneca'];
+
+arrayFilmes = ['Parasita', 'Ex_Machina', 'Divertida Mente', 'Forrest Gump'];
+arrayNotas = [10, 7, 9, 8];
+
+arrayFilm = ['Os Vingadores','Os 3 patetas','Batman','SuperMan','IT a coisa','Carros'];
+
+vetorFilme = ['batman','hulk','superman'];
+
+vetorPais = ['Brasil','EUA','Espanha','Irlanda'];
 
 
 
