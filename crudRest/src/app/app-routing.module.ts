d26f8c7f94899d5a193fa205fc251a8fc57bffcf
Jorge Miguel <111158100+JorgeMiguelvs@users.noreply.github.com>
2023-05-26 22:24:19 -0300

    Angular Material

--- crudRest/src/app/app-routing.module.ts
+++ crudRest/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutoComponent } from './views/produto/produto.component';
+import { SobreComponent } from './views/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'produto', component:ProdutoComponent},
+  {path:'sobre', component:SobreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
--- crudRest/src/app/app.module.ts
+++ crudRest/src/app/app.module.ts
@@ -5,17 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ProdutoComponent } from './views/produto/produto.component';
-
+import { SobreComponent } from './views/sobre/sobre.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
-    ProdutoComponent
+    ProdutoComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
